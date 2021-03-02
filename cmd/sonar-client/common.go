ede54a894a296ca6d32848059e4f04a9be8add9e
Sunghyun Kim <cqbqdd11519@gmail.com>
2021-03-02 12:17:38 +0900

    [chore] Delete unnecessaries (#4)

--- cmd/sonar-client/common.go
+++
@@ -1,9 +0,0 @@
-package main
-
-const (
-	SonarResultPath = "/result-webhook"
-	SonarOutputPath = "/tekton/results/sonar-webhook-result"
-	WebhookKeyPath  = "/tekton/results/sonar-webhook-key"
-
-	WebhookPort = 8080
-)
